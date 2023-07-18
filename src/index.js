5171fc0634001f835c278f6673a5bd6116453142
elsa-kim <ksh96611@naver.com>
2023-07-18 21:05:12 +0900

    firebase Auth

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
-import { firebase } from "./firebase";
-console.log(firebase);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
