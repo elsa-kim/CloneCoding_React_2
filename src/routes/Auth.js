4797d5bc339a55bb8a0242630fc203a1e4f9d2f3
elsa-kim <ksh96611@naver.com>
2023-07-24 21:07:04 +0900

    firebase 에러 해결중,,

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
   const onChange = (e) => {
     const {
       target: { name, value },
@@ -13,15 +17,29 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmit = (e) => {
+
+  const onSubmit = async (e) => {
     e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create account
+        data = await createUserWithEmailAndPassword(auth, email, password);
+      } else {
+        // log in
+        data = await signInWithEmailAndPassword(auth, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
         <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
       </form>
       <div>
         <button>Continue with Google</button>
