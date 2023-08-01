13df7bbd3e2a2368eb5949bf7efb06b434277a7a
elsa-kim <ksh96611@naver.com>
2023-08-01 23:10:25 +0900

    로그인 UI 상태 수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { authService } from "../fbase";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
 export const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <BrowserRouter>
       <Routes>{isLoggedIn ? <Route path="/" element={<Home />} /> : <Route path="/" element={<Auth />} />}</Routes>
