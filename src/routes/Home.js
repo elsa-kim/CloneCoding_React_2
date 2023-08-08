f2da322dc61bc6c2f822ef3815a75d7ca51f5f77
elsa-kim <ksh96611@naver.com>
2023-08-08 21:38:05 +0900

    getting tweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
 import { dbService } from "../fbase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { useEffect } from "react";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const getTweets = async () => {
+    const dbTweets = await getDocs(collection(dbService, "tweets"));
+    dbTweets.forEach((document) => {
+      const tweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTweets((prev) => [tweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -27,6 +42,13 @@ const Home = () => {
         <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
         <input type="submit" value="Tweet" />
       </form>
+      <div>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <h4>{tweet.tweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
